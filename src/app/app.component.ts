4468bca72ba806a669c2015db002e49d9aad57be
StefanoZambolin <70204526+StefanoZambolin@users.noreply.github.com>
2024-03-09 22:31:59 +0100

    ngrx signal store crash course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosStore } from './store/todos.store';
+import { JsonPipe } from '@angular/common';
+import { TodosListComponent } from './todos-list/todos-list.component';
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, TodosListComponent, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ngrx-signal-store-crash-course';
+export class AppComponent implements OnInit {
+ store = inject(TodosStore);
+
+ ngOnInit(): void {
+   this.loadTodos().then( () => console.log("Todos loaded!"));
+ }
+
+ async loadTodos() {
+  await this.store.loadAll();
+ }
 }
